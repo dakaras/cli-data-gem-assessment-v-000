778026f685831af2ef506e8c7d8457457b924009
Daniel Karagiannis <dakaragiannis@gmail.com>
2017-10-30 20:24:13 +0000

    added list_quotes method template

--- stock_screener/lib/stock_screener/cli.rb
+++ stock_screener/lib/stock_screener/cli.rb
@@ -2,6 +2,13 @@
 class StockScreener::CLI
 
   def call
-    puts "NYSE Quotes For Today's Market Session."
+    puts "Gold Miner Quotes For Today's Market Session:"
+    list_quotes
+  end
+
+  def list_quotes
+    puts <<-DOC.gsub /^\s+/, ""
+    1. TICKER:  NEM  NAME:  Newmont Mining  PRICE:  $36.23  CHANGE: 1.20%  VOLUME: 1,807,123
+    DOC
   end
 end
