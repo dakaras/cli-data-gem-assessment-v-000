ebf4323c100eb5862022040f18886de1985df7f5
Daniel Karagiannis <dakaragiannis@gmail.com>
2017-10-30 20:38:45 +0000

    added menu method template

--- stock_screener/lib/stock_screener/cli.rb
+++ stock_screener/lib/stock_screener/cli.rb
@@ -4,11 +4,24 @@ class StockScreener::CLI
   def call
     puts "Gold Miner Quotes For Today's Market Session:"
     list_quotes
+    menu
   end
 
   def list_quotes
     puts <<-DOC.gsub /^\s+/, ""
     1. TICKER:  NEM  NAME:  Newmont Mining  PRICE:  $36.23  CHANGE: 1.20%  VOLUME: 1,807,123
     DOC
+  end
+
+  def menu
+    puts "Enter the number of the quote you would like more data on."
+    input = gets.strip
+
+    case input
+    when "1"
+      puts "MARKET CAP:   DIVIDEND:    P/E:   ROE:    ROA:    NET PROFIT MARGIN:    PERF YTD: "
+    when "2"
+      puts "MARKET CAP:   DIVIDEND:    P/E:   ROE:    ROA:    NET PROFIT MARGIN:    PERF YTD: "
+    end
   end
 end
